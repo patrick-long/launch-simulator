edcf27f4ce857c89767ceb121250ad3dc9046b7e
Patrick Long <patrick@deepspacerobots.com>
2023-07-10 11:50:57 -0500

    create new simulator route

---
+++ src/routes/simulator/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Launch Simulator</title>
+	<meta name="description" content="Launch simulator" />
+</svelte:head>
+
+<section class="main-content">
+	<h1>Simulator</h1>
+</section>
+
+<style>
+	.main-content {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+		height: 100%;
+		width: 100%;
+		background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.75)),
+			url('$lib/images/about.png');
+		background-size: cover;
+	}
+</style>
