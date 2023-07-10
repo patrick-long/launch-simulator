92bebc20580351a817415aeac272f182e6e2e9c6
Patrick Long <patrick@deepspacerobots.com>
2023-07-10 11:51:15 -0500

    add link from home page to simulator and add simple styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+	const handleClick = (): void => {};
+</script>
+
 <svelte:head>
 	<title>Launch Simulator</title>
 	<meta name="description" content="Rocket launch simulator" />
 </svelte:head>
 
-<section class="main-section" />
+<section class="main-section">
+	<a class="btn-link secondary" href="/simulator">Start Simulator</a>
+</section>
 
 <style>
 	section {
@@ -20,8 +26,7 @@
 		height: 100%;
 		width: 100%;
 		background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75)),
-			url('$lib/images/launch.jpg');
-		/* filter: blur(3px); */
+			url('$lib/images/landing-page.png');
 		-webkit-background-size: cover;
 		-moz-background-size: cover;
 		-o-background-size: cover;
